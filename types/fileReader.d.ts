fef542b8ed3ce99287ea5d664f78e7858c4c63c5
zzoeyh <2694035366@qq.com>
2024-12-22 23:07:35 +0800

    fix: 修改gitignore

--- types/fileReader.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function extractFileFromZip(zipDataFiles: any, path: any): Promise<any>;
-export declare function extractFilesFromZip(file: Blob | ArrayBuffer, filePaths: string[]): Promise<{
-    [key: string]: string | null;
-}>;
-export declare function checkFileExistInZip(zipDataFiles: any, path: any): boolean;
-export declare function checkFilesExistInZip(file: any, filePaths: any): Promise<{}>;
